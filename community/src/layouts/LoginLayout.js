c32c54074dd1890a17ebc09757807fd644744763
Glassay <31647920+Glassay@users.noreply.github.com>
2017-12-11 19:27:57 +0800

    q

---
+++ community/src/layouts/LoginLayout.js
@@ -0,0 +1,17 @@
+/**
+ * 2017-12-11 Glassay & Kidder
+ * 论坛登录界面
+ * CSS By Grommet
+ */
+
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <h1>123</h1>
+    );
+  }
+}
+
+export default Login;
