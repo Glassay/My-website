c2bad5ea3ac0fbf00eaeea56cbd588d1d85ee299
kidder <841497533@qq.com>
2017-12-12 19:54:47 +0800

    mainlayout

--- community/src/comtens/Card.js
+++ community/src/comtens/Card.js
@@ -9,16 +9,23 @@ class Card extends React.Component {
       <div className={styles.card}>
         <div className={styles.title} >
           <Row type="flex" justify="space-between" align="bottom">
-            <div className={styles.h}>标题</div>
+            <div className={styles.topic}>突然想起你，我笑了笑自己</div>
             <Row type="flex" justify="space-between" align="middle">
               <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" className={styles.h} />
-              <div className={styles.h}>user</div>
-              <div className={styles.h}>Date</div>
+              <div className={styles.h}>Kidder</div>
+              <div className={styles.h}>2017-10-09</div>
             </Row>
           </Row>
         </div>
         <Divider className={styles.divider} />
-        <div className={styles.text}>123</div>
+        <div className={styles.text}>夜色微凉，皎洁的月光缓缓地倾泻下来，与昏黄的路灯融为一体。
+
+时隔多年，我再次从这里经过。街角的路边摊早已换了人家，空气中弥漫着的，还是记忆中炸串的味道。
+
+穿过这条小巷，高楼林立的大厦在暗夜里深沉地伫立着，好似这座城市的守卫兵，一切都是最初的模样。
+
+此刻，我走在路的转角，突然想起她。按照记忆里再熟悉不过的路线，摸索到那条走了无数次的路。
+</div>
         <div>
           <div className={styles.cardicon}>
             <Row type="flex" justify="space-between" align="middle" style={{ width: '14%' }} >
--- community/src/layouts/BasicLayout.js
+++ community/src/layouts/BasicLayout.js
@@ -5,7 +5,8 @@
 
 import React from 'react';
 // import 'semantic-ui-css/semantic.min.css';
-import { Layout, Avatar, Row } from 'antd';
+import { Layout, Avatar, Row, Button } from 'antd';
+import { Link, Switch, Route } from 'dva/router';
 import styles from './BasicLayout.less';
 import Card from '../comtens/Card';
 
@@ -19,22 +20,32 @@ class MainLayout extends React.Component {
         <Layout className={styles.color}>
           <Header className={styles.header}>
             <Row type="flex" justify="space-between" align="middle">
-              <div className={styles.title}>Todpole‘Community</div>
-              <Row type="flex" justify="space-between" align="middle" style={{ width: '10%', marginRight: '10%' }} >
-                <div className={styles.fatie}>发帖</div>
+              <Link to="/text">
+                <div className={styles.title}>Todpole‘Community</div>
+              </Link>
+              <Row type="flex" justify="space-between" align="middle" style={{ width: '14%', marginRight: '10%' }} >
+                <Link to="/text">
+                  <Button icon="edit" type="primary" className={styles.button}>发帖</Button>
+                </Link>
                 <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size="large" className={styles.icon} />
               </Row>
             </Row>
           </Header>
           <Content style={{ width: '60%', margin: 'auto' }} >
-            <div style={{ padding: 24, minHeight: 280 }} className={styles.color}>
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-            </div>
+            <Switch>
+              <Route
+                path="/m" render={() =>
+                  <div style={{ padding: 24, minHeight: 280 }} className={styles.color}>
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                  </div>}
+              />
+              <Route path="/text" render={() => <h1>aa</h1>} />
+            </Switch>
           </Content>
           <Footer style={{ textAlign: 'center' }} className={styles.color}>
             华北电力大学科技学院论坛
