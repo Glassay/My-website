89418eca1e8abfe449e98776ccf703aebdf7e622
kidder <841497533@qq.com>
2017-12-12 17:05:41 +0800

    router

--- community/src/router.js
+++ community/src/router.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-// import Login from './layouts/LoginLayout';
+import Login from './layouts/LoginLayout';
 import MainLayout from './layouts/BasicLayout';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={MainLayout} />
+        <Route path="/" exact component={Login} />
+        <Route path="/m" exact component={MainLayout} />
       </Switch>
     </Router>
   );
