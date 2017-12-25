bee12fcaa636cf1d7600f8e1130ab41f51b0cc55
Glassay <31647920+Glassay@users.noreply.github.com>
2017-12-25 16:34:36 +0800

    获取发帖的状态

--- community/src/component/edit.js
+++ community/src/component/edit.js
@@ -10,12 +10,47 @@ import styles from './edit.less';
 
 const { TextArea } = Input;
 class Edit extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      article_title: '',
+      article_content: '',
+    };
+  }
+
+  componentDidMount() {
+    this.title.focus();
+  }
+
+  handleTitleChange(event) {
+    this.setState({
+      article_title: event.target.value,
+    });
+  }
+
+  handleContentChange(event) {
+    this.setState({
+      article_content: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
-        <Input placeholder="题目" className={styles.tip} />
+        <Input
+          placeholder="题目"
+          className={styles.tip}
+          value={this.state.article_title}
+          onChange={this.handleTitleChange.bind(this)}
+          ref={(title) => { this.title = title; }}
+        />
         <Divider />
-        <TextArea placeholder="内容" rows={23} />
+        <TextArea
+          placeholder="内容"
+          rows={23}
+          value={this.state.article_content}
+          onChange={this.handleContentChange.bind(this)}
+        />
         <Divider />
         <Row type="flex" justify="end">
           <Button icon="rocket" type="primary" className={styles.up}>发布</Button>
